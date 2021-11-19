07bc38dc3eb91485a0c357aa7bb09d955ac170cc
Benjamin Franzke <bfr@qbus.de>
2021-11-19 10:37:48 +0100

    Fix Modal.advanced return value
    Ensure that original modal jquery object is returned, not the .modal-content children
    
    Fixes #2

--- Resources/Public/JavaScript/ResizableModals.js
+++ Resources/Public/JavaScript/ResizableModals.js
@@ -1,13 +1,14 @@
 define(['TYPO3/CMS/Backend/Modal', 'jquery-ui/draggable', 'jquery-ui/resizable'], function (Modal) {
     var originalAdvanced = Modal.advanced;
     Modal.advanced = function (configuration) {
-        return originalAdvanced.apply(Modal, [configuration])
-            .find('.modal-content')
+        var modal = originalAdvanced.apply(Modal, [configuration])
+        modal.find('.modal-content')
             .resizable({
                 handles: 'n, e, s, w, ne, sw, se, nw',
             })
             .draggable({
                 handle: '.modal-header'
             })
+	return modal;
     }
 });
